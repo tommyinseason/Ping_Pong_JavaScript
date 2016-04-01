f48a9ab9f54b1489ac81cf04ba4069e798616384
Tommy Jones <tommyinseason@yahoo.com>
2016-04-01 13:38:37 -0700

    take users input number and define factors of 3 and 5 with ping and pong

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Business Logic Section //
+      function pingPong (num) {
+        for ( var i = 1; i <= num; i++){
+          $(".output").append("<li>" + results(i) + "</li>");
+  }
+}
+      function results(integer) {
+        var show = "";
+        if (integer % 3 === 0) {
+          show = show.concat("ping");
+        }
+        if (integer % 5 === 0) {
+          show = show.concat("pong");
+        }
+        if (!show) {
+          show = integer.toString();
+        }
+        return show;
+}
+// User Interface Logic //
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+      event.preventDefault();
+      $(".output").text("");
+      var integer = parseInt($("#integer").val());
+      pingPong(integer);
+    });
+  });
